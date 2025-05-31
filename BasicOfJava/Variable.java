de04ce61790974f2eca8102322fcf30d082e381c
Ranjan Kumar <154871097+Rksingh9546@users.noreply.github.com>
2025-05-31 17:57:23 +0530

    Add files via upload

---
+++ BasicOfJava/Variable.java
@@ -0,0 +1,12 @@
+package BasicOfJava;
+
+public class Variable {
+    public static void main(String[] args) {
+        int x =5;
+        System.out.println(x);
+        x =7;
+        System.out.println(x);
+        x =x + 6;
+        System.out.println(x);
+    }
+}
